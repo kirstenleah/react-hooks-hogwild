2630a29a9b48609566f4b5c31de34a663a0ade68
Kirsten <kirsten.staggs@gmail.com>
2022-05-03 09:30:49 -0600

    Done.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,38 +1,53 @@
 import React, { useState } from "react";
 
-function Form() {
-  const [name, setName] = useState("");
+function Form({ hogs, setCurrentHogs }) {
+  const [searchName, setSearchName] = useState("");
   const [weight, setWeight] = useState("");
   const [greased, setGreased] = useState(false);
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    const formData = {
-      name,
-      weight,
-      greased,
-    };
+  function handleNameInput(e) {
+    e.preventDefault();
+    const searchedHog = hogs.filter((hog) => {
+      if (hog.name.includes(searchName)) {
+        return hog;
+      }
+    });
+    console.log(searchedHog);
+    setCurrentHogs(searchedHog);
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleNameInput}>
       <h3>Find a Hog</h3>
-      <label htmlFor="name">Name</label>
-      <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} />
-
-      <label htmlFor="weight">Weight</label>
-      <select id="weight" value={weight} onChange={(e) => setWeight(e.target.value)}>
-        <option value="small">0-2 lb</option>
-        <option value="medium">2-4 lb</option>
-        <option value="large">4+ lb</option>
-      </select>
-
-      <label>
-        Show Greased Only
-        <input type="checkbox" id="greased" checked={greased} onChange={(e) => setGreased(e.target.value)} />
+      <label htmlFor="name">
+        Name
+        <input type="text" id="name" value={searchName} onChange={(e) => setSearchName(e.target.value)} />
       </label>
+      <input type="submit" />
     </form>
   );
-}
+} // enf function
 
 export default Form;
+
+//   function handleSubmit(event) {
+//     event.preventDefault();
+//     const formData = {
+//       name,
+//       weight,
+//       greased,
+//     };
+//   }
+
+// <label htmlFor="weight"> Weight</label>
+// <select id="weight" value={weight} onChange={(e) => setWeight(e.target.value)}>
+//   <option value="small">0-2 lb</option>
+//   <option value="medium">2-4 lb</option>
+//   <option value="large">4+ lb</option>
+// </select>
+
+// <label>
+//   {"   "}
+//   Show Greased Only
+//   <input type="checkbox" id="greased" checked={greased} onChange={(e) => setGreased(e.target.checked)} />
+// </label>
--- src/components/HogTile.js
+++ src/components/HogTile.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-function HogTile({ name, specialty, greased, weight, image }) {
+function HogTile({ hogs }) {
   let [isDetailed, setIsDetailed] = useState(false);
 
   function handleClick() {
@@ -10,16 +10,17 @@ function HogTile({ name, specialty, greased, weight, image }) {
   const details =
     isDetailed === true ? (
       <>
-        <h3 className="details"> Specialty: {specialty}</h3>
-        <h3 className="details"> Weight: {weight} lbs.</h3>
-        <h3 className="details">{greased ? "Greased" : "Not Greased"}</h3>
+        <h3 className="details"> Specialty: {hogs.specialty}</h3>
+        <h3 className="details"> Weight: {hogs.weight} lbs.</h3>
+        <h3 className="details"> Medal: {hogs["highest medal achieved"]} </h3>
+        <h3 className="details">{hogs.greased ? "Greased" : "Not Greased"}</h3>
       </>
     ) : null;
 
   return (
     <div className="pigTile" onClick={handleClick}>
-      <h2>{name}</h2>
-      <img src={image} alt={name} />
+      <h2>{hogs.name}</h2>
+      <img height="100" width="100" src={hogs.image} alt={hogs.name} />
       {details}
     </div>
   );
