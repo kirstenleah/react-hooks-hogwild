7ec2d0876963d88d43437659d831fe6a79f01e8a
Kirsten <kirsten.staggs@gmail.com>
2022-05-03 10:15:28 -0600

    Done.

--- src/components/Form.js
+++ src/components/Form.js
@@ -16,6 +16,25 @@ function Form({ hogs, setCurrentHogs }) {
     setCurrentHogs(searchedHog);
   }
 
+  function handleWeight(e) {
+    const weightSelection = e.target.value;
+    console.log(weightSelection);
+    const weightClass = hogs.filter((hog) => {
+      if (weightSelection === "small" && hog.weight <= 2) {
+        console.log("inside the if");
+        console.log(hog);
+        return hog;
+      } else if (weightSelection === "medium" && hog.weight >= 2 && hog.weight <= 4) {
+        return hog;
+      } else if (weightSelection === "large" && hog.weight >= 4) {
+        return hog;
+      } else if (weightSelection === "default") {
+        return setCurrentHogs(hogs);
+      }
+    });
+    setCurrentHogs(weightClass);
+  }
+
   function handleGreasedChecked(e) {
     setGreased(!greased);
     if (e.target.checked) {
@@ -33,7 +52,13 @@ function Form({ hogs, setCurrentHogs }) {
         Name
         <input type="text" id="name" value={searchName} onChange={(e) => setSearchName(e.target.value)} />
       </label>
-      <input type="submit" />{" "}
+      <input type="submit" /> <label htmlFor="weight"> Weight</label>
+      <select id="weight" value={weight} onChange={handleWeight}>
+        <option value="default">Select Weight</option>
+        <option value="small">0-2 lb</option>
+        <option value="medium">2-4 lb</option>
+        <option value="large">4+ lb</option>
+      </select>
       <label>
         {"   "}
         Show Greased Only <input type="checkbox" id="greased" checked={greased} onChange={handleGreasedChecked} />
@@ -52,10 +77,3 @@ export default Form;
 //       greased,
 //     };
 //   }
-
-// <label htmlFor="weight"> Weight</label>
-// <select id="weight" value={weight} onChange={(e) => setWeight(e.target.value)}>
-//   <option value="small">0-2 lb</option>
-//   <option value="medium">2-4 lb</option>
-//   <option value="large">4+ lb</option>
-// </select>
